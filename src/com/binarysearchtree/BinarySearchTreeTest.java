220989c6f91ac96509b2633c1518b405719f8865
aditya Kharade <adityakharade51@gmail.com>
2020-10-16 11:20:32 +0530

    Added : added functionality to search a particular node in tree

--- src/com/binarysearchtree/BinarySearchTreeTest.java
+++ src/com/binarysearchtree/BinarySearchTreeTest.java
@@ -51,4 +51,48 @@ class BinarySearchTreeTest {
 		bst.add(67);
 		assertEquals(13, bst.findSize());
 	}
+
+	/**
+	 * Searching particular node in the tree
+	 */
+	@Test
+	void givenNode_WhenSearchedInTree_ShouldReturnTrue() {
+		MyBinarySearchTree<Integer> bst = new MyBinarySearchTree<>();
+		bst.add(56);
+		bst.add(30);
+		bst.add(70);
+		bst.add(22);
+		bst.add(40);
+		bst.add(11);
+		bst.add(3);
+		bst.add(16);
+		bst.add(60);
+		bst.add(95);
+		bst.add(65);
+		bst.add(63);
+		bst.add(67);
+		assertTrue(bst.searchKey(63));
+	}
+
+	/**
+	 * Checking whether the node exist or not
+	 */
+	@Test
+	void givenNode_WhenSearchedInTree_ShouldReturnFalse() {
+		MyBinarySearchTree<Integer> bst = new MyBinarySearchTree<>();
+		bst.add(56);
+		bst.add(30);
+		bst.add(70);
+		bst.add(22);
+		bst.add(40);
+		bst.add(11);
+		bst.add(3);
+		bst.add(16);
+		bst.add(60);
+		bst.add(95);
+		bst.add(65);
+		bst.add(63);
+		bst.add(67);
+		assertFalse(bst.searchKey(20));
+	}
 }
--- src/com/binarysearchtree/MyBinarySearchTree.java
+++ src/com/binarysearchtree/MyBinarySearchTree.java
@@ -28,4 +28,20 @@ public class MyBinarySearchTree<K extends Comparable<K>> {
 	public int getSize(MyBinaryNode<K> current) {
 		return (current == null) ? 0 : 1 + getSize(current.leftNode) + getSize(current.rightNode);
 	}
+
+	public boolean searchKey(K key) {
+		return searchInTree(root, key);
+	}
+
+	public boolean searchInTree(MyBinaryNode<K> current, K key) {
+		if (current == null) {
+			return false;
+		}
+		if (key.compareTo(current.key) == 0) {
+			return true;
+		} else if (key.compareTo(current.key) < 0) {
+			return searchInTree(current.leftNode, key);
+		} else
+			return searchInTree(current.rightNode, key);
+	}
 }
