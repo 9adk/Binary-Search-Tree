0d181dc2720bd7277337b778ce577f2376ed6897
aditya Kharade <adityakharade51@gmail.com>
2020-10-16 11:07:42 +0530

    Added : added test case for checking size of binary search tree

--- src/com/binarysearchtree/BinarySearchTreeTest.java
+++ src/com/binarysearchtree/BinarySearchTreeTest.java
@@ -29,4 +29,26 @@ class BinarySearchTreeTest {
 		int result = bst.findSize();
 		assertEquals(3, result);
 	}
+
+	/**
+	 * Creating binary search tree and checking it's size
+	 */
+	@Test
+	void givenNodes_WhenAddedToTree_ShouldReturnSize() {
+		MyBinarySearchTree<Integer> bst = new MyBinarySearchTree<>();
+		bst.add(56);
+		bst.add(30);
+		bst.add(70);
+		bst.add(22);
+		bst.add(40);
+		bst.add(11);
+		bst.add(3);
+		bst.add(16);
+		bst.add(60);
+		bst.add(95);
+		bst.add(65);
+		bst.add(63);
+		bst.add(67);
+		assertEquals(13, bst.findSize());
+	}
 }
